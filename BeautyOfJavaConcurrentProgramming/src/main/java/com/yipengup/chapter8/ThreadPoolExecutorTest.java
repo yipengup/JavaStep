2df0a140699ef21e50aa7ea364c1196667ab8ac5
yipengup <844579582@qq.com>
2022-01-25 16:38:07 +0800

    添加线程池中处理任务的流程案例

--- BeautyOfJavaConcurrentProgramming/src/main/java/com/yipengup/chapter8/ThreadPoolExecutorTest.java
+++ BeautyOfJavaConcurrentProgramming/src/main/java/com/yipengup/chapter8/ThreadPoolExecutorTest.java
@@ -12,7 +12,8 @@ public class ThreadPoolExecutorTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        printPoolInfoForThreadOverflow();
+        // printPoolInfoForThreadOverflow();
+        queueOrMaximumPoolPriority();
     }
 
     /**
@@ -63,5 +64,35 @@ public class ThreadPoolExecutorTest {
         handler.rejectedExecution(null, executor);
     }
 
+    /**
+     * 验证任务是进入队列还是先创建线程到最大线程个数
+     * <p>
+     * 结论： 先创建核心线程，在将多余的任务放入到队列中，队列满后在创建线程到最大线程数
+     */
+    private static void queueOrMaximumPoolPriority() {
+        Runnable task = () -> {
+            System.out.println(Thread.currentThread().getName() + "running");
+            for (; ; ) {
+
+            }
+        };
+
+        // 核心线程 = 1， 最大线程 = 2， 队列 = 5
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 2, 0, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(5), NamedThreadFactory.create("queueOrMaximumPoolPriority"), new ThreadPoolExecutor.DiscardPolicy());
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("=====================");
+            executor.submit(task);
+            // 返回正在主动执行任务的线程的大概数量
+            System.out.println("active thread = " + executor.getActiveCount());
+            // 返回队列的大小
+            System.out.println("queue size = " + executor.getQueue().size());
+            System.out.println("task Count = " + executor.getTaskCount());
+            System.out.println("task Complement = " + executor.getCompletedTaskCount());
+            System.out.println("=====================");
+        }
+
+    }
 
 }
